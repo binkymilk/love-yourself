bd7aed67daa80e62be631dcb6cf7e29c642e1bd3
Bianca Silmaro <bianca@thinkingmachin.es>
2019-01-28 01:10:29 +0800

    Update data and links template

---
+++ data.js
@@ -0,0 +1,240 @@
+var lyrics = [
+  {
+    lyric: 'On days where I hate myself for being me, on days where I want to disappear forever. Let’s make a door. It’s in your heart. Open the door and this place will await, Magic Shop.',
+    song: 'Magic Shop',
+    spotify: 'https://open.spotify.com/track/22ryCgQttpV7oCrn7llGRu?si=4vJbZXYpSWen3NlI1VlbiQ',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'It’s okay come on when I say one two three forget it. Erase all sad memories. Smile holding onto each other’s hands.',
+    song: '둘! 셋! (그래도 좋은 날이 더 많기를) [2! 3! (Still Wishing For More Good Days)]',
+    spotify: 'https://open.spotify.com/track/0ma7lbs8BsVIv2kSaqOsDI?si=hxlbgjdnTZaKugWYtB_6IQ',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'All you with a lot of fear, come here. All you who are suffering, come here. Lift up your fists, all night long. With marching footsteps, jump and go crazy',
+    song: '불타오르네 [Burning Up (Fire)]',
+    spotify: 'https://open.spotify.com/track/4z9gBZQjGS4QLb4LOvmeuA?si=EQrLXl0sTSS-4eOf2GZ6Rg',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'You should rather die than lose. You have to win, you, you, you. Falling short is such a painful thing. If you don’t go through it then you can’t know it',
+    song: '어긋 [uhgood]',
+    spotify: 'https://open.spotify.com/track/7hjWFO88DN3FroDMRIcehn?si=DtB98aCLRrmkPXYHr1iJ9w',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'I’m living because I can’t die',
+    song: 'So Far Away',
+    spotify: 'https://open.spotify.com/track/41sIKltzKnhFsDXWTKjO3R?si=pILc8gEWRcqP1JqHeCXk7A',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'Why do you keep trying to hide it under your mask when even the scars left from my mistakes are all my constellations',
+    song: 'Answer: Love Myself',
+    spotify: 'https://open.spotify.com/track/4eVkn18rN4GguUWOsR6Use?si=nbqbRX9pRDO8ARKQN_eDGg',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'Yesterday’s me, today’s me, tomorrow’s me (I’m learning how to love myself) with no exception, nothing left behind, all of it is me.',
+    song: 'Answer: Love Myself',
+    spotify: 'https://open.spotify.com/track/4eVkn18rN4GguUWOsR6Use?si=nbqbRX9pRDO8ARKQN_eDGg',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'It’s alright to stop. There’s no need to run without even knowing the reason.',
+    song: '낙원 [Paradise]',
+    spotify: 'https://open.spotify.com/track/01dmH2IPkrLNWIMPNXlreE?si=91HCXMMQSDSMROzEOpyqEg',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'It’s alright to not have a dream, if you have moments where you feel happiness for a while.',
+    song: '낙원 [Paradise]',
+    spotify: 'https://open.spotify.com/track/01dmH2IPkrLNWIMPNXlreE?si=91HCXMMQSDSMROzEOpyqEg',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'Who says a dream must be something grand? Just become anybody. We deserve a life. Whatever big or small, you are you after all.',
+    song: '낙원 [Paradise]',
+    spotify: 'https://open.spotify.com/track/01dmH2IPkrLNWIMPNXlreE?si=91HCXMMQSDSMROzEOpyqEg',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'It all passes. Someday. For sure. Certainly.',
+    song: '지나가 [everythingoes]',
+    spotify: 'https://open.spotify.com/track/7DKYA93lgKbhxzqE2ojtCS?si=SMpkieEISmypYN2NhL0gtg',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'Just like the night leaves and the morning comes. The spring leaves and summer comes, but just like the flower wilts and the fruit grows ripe, everything needs to go through pain.',
+    song: '지나가 [everythingoes]',
+    spotify: 'https://open.spotify.com/track/7DKYA93lgKbhxzqE2ojtCS?si=SMpkieEISmypYN2NhL0gtg',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'In a sigh, lots of worries are hiding. Stop thinking about it, you already know it all. In the middle of the road, in the moment you want to give up, shout out louder, so what.',
+    song: 'So What',
+    spotify: 'https://open.spotify.com/track/4CvId64YZSB0RfRdfzPIQU?si=By6fKFN6TnW8PQ1_Ds3bCQ',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: '90% of your worries are an imaginary swamp that you created. Just go instead of worrying, Don’t get scared, cheer up.',
+    song: 'So What',
+    spotify: 'https://open.spotify.com/track/4CvId64YZSB0RfRdfzPIQU?si=By6fKFN6TnW8PQ1_Ds3bCQ',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'The morning will come again. No darkness, no season is lasts forever.',
+    song: '봄날 [Spring Day]',
+    spotify: 'https://open.spotify.com/track/0WNGsQ1oAuHzNTk8jivBKW?si=WEet3h8rSSy4Ak46CNatgA',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'Dream, you will fully bloom after all the hardships. Dream, though your beginnings may be humble, may the end be prosperous.',
+    song: 'So Far Away',
+    spotify: 'https://open.spotify.com/track/41sIKltzKnhFsDXWTKjO3R?si=pILc8gEWRcqP1JqHeCXk7A',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'These wings sprouted from my pain. But these wings are going towards the light. Even if it’s tiring and painful, I will fly if I can.',
+    song: 'A Supplementary Story: You Never Walk Alone',
+    spotify: 'https://open.spotify.com/track/0rwNiysHvEWZWendzWyadr?si=SBfrS4wNSCqgyR4pXwiEFw',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'Where there is hope, there are trials. We need to be in despair for all of our hardships.',
+    song: 'Sea',
+    spotify: '',
+    youtube: 'https://www.youtube.com/watch?v=z4gSWMEz4Nc',
+    soundcloud: ''
+  },
+  {
+    lyric: 'I have a long way to go so why am I running in place? I scream out of frustration but the empty air echoes. I hope tomorrow will be different from today. I’m just wishing.',
+    song: 'Tomorrow',
+    spotify: 'https://open.spotify.com/track/1n99s09TM651WSXOMxyoHF?si=tQj_RtuzTkOd7BBazII3nQ',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: '(So thanks) for believing in me, for handling these tears and scars. (So thanks) for being my light, for being a flower in the most beautiful moment of life.',
+    song: '둘! 셋! (그래도 좋은 날이 더 많기를) [2! 3! (Still Wishing For More Good Days)]',
+    spotify: 'https://open.spotify.com/track/0ma7lbs8BsVIv2kSaqOsDI?si=hxlbgjdnTZaKugWYtB_6IQ',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'If you feel like you’re going to crash, step harder, kid.',
+    song: 'INTRO: Nevermind',
+    spotify: 'https://open.spotify.com/track/5kKoQjyLorcc6CK6GVslx8?si=1ZQxRJtvQWybp17lGjQOig',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'Because the dawn right before the sun rises is the darkest.',
+    song: 'Tomorrow',
+    spotify: 'https://open.spotify.com/track/1n99s09TM651WSXOMxyoHF?si=tQj_RtuzTkOd7BBazII3nQ',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'Dreams, hopes, forward, forward.',
+    song: 'EPILOGUE: Young Forever',
+    spotify: 'https://open.spotify.com/track/43hiD0QEeUysuWqDzd8GwA?si=IvIavoz7T-e-dzp4Rg3nqw',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'Until this cold winter ends, and the spring comes again, and until the flowers bloom again. Please stay there a little longer. Please stay there.',
+    song: '봄날 [Spring Day]',
+    spotify: 'https://open.spotify.com/track/0WNGsQ1oAuHzNTk8jivBKW?si=WEet3h8rSSy4Ak46CNatgA',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'But let’s laugh and talk and go against the wind. Let’s run stupidly like a fool sometimes. Within the mistakes and tears we just go.',
+    song: 'So What',
+    spotify: 'https://open.spotify.com/track/4CvId64YZSB0RfRdfzPIQU?si=By6fKFN6TnW8PQ1_Ds3bCQ',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'Until then I’ll combat the pains and scars until I learn to embrace the concept of bending without breaking/',
+    song: 'Path',
+    spotify: '',
+    youtube: 'https://www.youtube.com/watch?v=DelZGaPxFOg',
+    soundcloud: ''
+  },
+  {
+    lyric: 'Even in the far future, never forget the you of right now. Wherever you are right now, you’re just taking a break. Don’t give up, you know. Don’t get too far away, tomorrow.',
+    song: 'Tomorrow',
+    spotify: 'https://open.spotify.com/track/1n99s09TM651WSXOMxyoHF?si=tQj_RtuzTkOd7BBazII3nQ',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'I’m living for the sake of understanding the world. But why hasn’t this world tried to understand me?',
+    song: 'Always',
+    spotify: '',
+    youtube: '',
+    soundcloud: 'https://soundcloud.com/bangtan/always-by-rm'
+  },
+  {
+    lyric: 'Maybe I, I can’t touch the sky but I want to stretch my arm. I want to run just a little bit.',
+    song: 'Awake',
+    spotify: 'https://open.spotify.com/track/0UDCfleTgwihlnOUxbzokR?si=Y9cUPGgSTqO-zb84JNzz4Q',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'I can have a taste of failure and frustration and bow my head. We are still young and immature, don’t even worry about it. Moss surely grows on a stone that doesn’t roll. If you can’t return, go straight through your mistakes and forget them all. Never mind.',
+    song: 'INTRO: Nevermind',
+    spotify: 'https://open.spotify.com/track/5kKoQjyLorcc6CK6GVslx8?si=1ZQxRJtvQWybp17lGjQOig',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'Rap doesn’t have to be fast and neither does life.',
+    song: 'So 4 More',
+    spotify: '',
+    youtube: '',
+    soundcloud: 'https://soundcloud.com/bangtan/so-4-more-by-bts'
+  },
+  {
+    lyric: 'Even if I’m slow I will walk with my own feet, because I know this path is mine to take, I will never lose my dream.',
+    song: 'Lost',
+    spotify: 'https://open.spotify.com/track/44F7Ieb36A6B2mXgrJLdR9?si=QRTFtQpYTCG-v3iQWufBcQ',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'Throw away the fear in your eyes, break up the glass ceiling that cages you. Until the day of victory, don’t kneel, don’t fall down.',
+    song: 'Not Today',
+    spotify: 'https://open.spotify.com/track/4hrpoqJMVE7FxBu1YWx0eQ?si=1lB1qXucT4yOTMoXEmUurA',
+    youtube: '',
+    soundcloud: ''
+  },
+  {
+    lyric: 'Reject rejection.',
+    song: '쩔어 [Dope]',
+    spotify: 'https://open.spotify.com/track/4TiKZrN7gtc62SUiufpFCM?si=c7qEQtgZRme78vp5MGtjHw',
+    youtube: '',
+    soundcloud: ''
+  }
+]
--- init.js
+++ init.js
@@ -1,22 +1,23 @@
-var lyrics = [
-  {
-    lyric: 'On days where I hate myself for being me, on days where I want to disappear forever. Let’s make a door. It’s in your heart. Open the door and this place will await, Magic Shop',
-    song: 'Magic Shop',
-    link: 'https://open.spotify.com/track/22ryCgQttpV7oCrn7llGRu?si=4vJbZXYpSWen3NlI1VlbiQ'
-  },
-  {
-    lyric: 'It’s okay come on when I say one two three forget it. Erase all sad memories. Smile holding onto each other’s hands',
-    song: '둘! 셋! (그래도 좋은 날이 더 많기를) [2! 3! (Still Wishing For More Good Days)]',
-    link: 'https://open.spotify.com/track/0ma7lbs8BsVIv2kSaqOsDI?si=hxlbgjdnTZaKugWYtB_6IQ'
-  }
-]
-
 function randomize () {
-  var num = Math.floor(Math.random() * lyrics.length)
+  if (lyrics) {
+    var num = Math.floor(Math.random() * lyrics.length)
 
-  document.getElementById('lyric').innerHTML = lyrics[num].lyric
-  var song = '— ' + lyrics[num].song
-  document.getElementById('song').innerHTML = song
-  document.getElementById('spotify').href = lyrics[num].link
-
+    document.getElementById('lyric').innerHTML = lyrics[num].lyric
+    var song = '— ' + lyrics[num].song
+    document.getElementById('song').innerHTML = song
+    var link = ''
+    var html = 'Play on &nbsp;'
+    if (lyrics[num].spotify) {
+      link = lyrics[num].spotify
+      html = html + '<i class="fab fa-spotify"></i>'
+    } else if (lyrics[num].youtube) {
+      link = lyrics[num].youtube
+      html = html + '<i class="fab fa-youtube"></i>'
+    } else if (lyrics[num].soundcloud) {
+      link = lyrics[num].soundcloud
+      html = html + '<i class="fab fa-soundcloud"></i>'
+    }
+    document.getElementById('link').href = link
+    document.getElementById('link').innerHTML = html
+  }
 }
